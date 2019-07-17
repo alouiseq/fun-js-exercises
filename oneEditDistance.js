c9d77b29cf1ba063e6ad8da28e311e7027962deb
Alouise Quiatchon <alouiseq@gmail.com>
2019-07-16 21:56:52 -0700

    add problem statement; uncomment test cases

--- oneEditDistance.js
+++ oneEditDistance.js
@@ -1,10 +1,15 @@
+/***
+ * Given two strings s and t, determine if they are both one edit distance apart
+ * One edit distance satisfies:
+ * insert, delete, or replace a character
+ */
 var isOneEditDistance = function(s, t) {
   // Keep two indices for each string
   // Use length of s OR t string and difference count to control loop
   // If char in s or t are different
   // - Find the longer string and increment index
   // - Compare the chars, return false if different, or continue
-  // After loop ends, check diff count and return false if !== 1, otherwise true
+  // After loop ends, check diff count and return false if count !== 1, otherwise true
   // O(n)
   let diffCount = 0;
   let i=0;
@@ -27,9 +32,9 @@ var isOneEditDistance = function(s, t) {
   return true;
 };
 
-// console.log(isOneEditDistance('ab', 'acb'));    // true
-// console.log(isOneEditDistance('cab', 'ad'));    // false
-// console.log(isOneEditDistance('1203', '1213')); // true
-// console.log(isOneEditDistance('abcd', 'abcd')); // false
-// console.log(isOneEditDistance('abcd', 'dcba')); // false
+console.log(isOneEditDistance('ab', 'acb'));    // true
+console.log(isOneEditDistance('cab', 'ad'));    // false
+console.log(isOneEditDistance('1203', '1213')); // true
+console.log(isOneEditDistance('abcd', 'abcd')); // false
+console.log(isOneEditDistance('abcd', 'dcba')); // false
 console.log(isOneEditDistance('', 'a')); // true
