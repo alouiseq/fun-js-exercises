8bde6c2ba1d46704cc6389caa85239b314963a47
Alouise Quiatchon <alouiseq@syapse.com>
2019-08-16 23:46:37 -0700

    add problem statement and solution via comments - 3sum

--- arrays_strings/3sum.js
+++ arrays_strings/3sum.js
@@ -1,12 +1,18 @@
+// Given an array nums of n integers, are there elements a, b, c in nums
+// such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+// The solution set must not contain duplicate triplets.
 var threeSum = function(nums){
   // loop over nums and store num as keys and count values
-  // sort nums
+  // sort nums(used to check for duplicate triplets)
   // Two loops (outer and inner) over nums
   // - decrement outer value in hash permanently
+  // - decrement inner value from hash
   // - add the values and find the value in hash that sum to 0
-  // - use a stack to increment/decrement visited values
-  //   - decrement if visited then increment after loop is done
+  // - use a stack to track visited values
+  // - use number string to check for duplicate triplets
   // - use zeroSums to store array of arrays of zero sums
+  // - after the end of inner loop, pop stack to reclaim hash values
+  // - traverse the next outer value and inner loop until the end of outer loop
   // O(n^2)
   const hash = {};
   const zeroSumsHash = {};
@@ -42,9 +48,9 @@ var threeSum = function(nums){
   return zeroSums;
 }
 
-// console.log(threeSum([-1, 0, 1, 2, -1, -4]));   // [[-1, 0, 1], [-1, -1, 2]]
-// console.log(threeSum([1, 1, -2]));   // [[1, 1, -2]]
-// console.log(threeSum([0,0,0,0]));   // [[0,0,0]]
-// console.log(threeSum([-1,1,0,-1,1,0,-1,1,0]));   // [[-1,1,0]]
-// console.log(threeSum([3,0,-2,-1,1,2]));   // [[-2,-1,3], [-2, 0, 2], [-1, 0, 1]]
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));   // [[-1, 0, 1], [-1, -1, 2]]
+console.log(threeSum([1, 1, -2]));   // [[1, 1, -2]]
+console.log(threeSum([0,0,0,0]));   // [[0,0,0]]
+console.log(threeSum([-1,1,0,-1,1,0,-1,1,0]));   // [[-1,1,0]]
+console.log(threeSum([3,0,-2,-1,1,2]));   // [[-2,-1,3], [-2, 0, 2], [-1, 0, 1]]
 console.log(threeSum([-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6]));   // len: 6
