49ba6e81bcc4edb5f717db9fe63fe5cc751c2573
Alouise Quiatchon <alouiseq@syapse.com>
2019-09-02 08:34:09 -0700

    add longestValidParen problem

---
+++ longestValidParen.js
@@ -0,0 +1,15 @@
+/*
+Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+*/
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParentheses = function(s) {
+    
+};
+
+const input = '(()';  // output: 2
+// const input = ')()())';  // output: 4
+
+console.log(longestValidParentheses(input));
