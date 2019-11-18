9a1380e5a85a625bacdc7363795f6f36860b7da6
Alouise Quiatchon <alouiseq@gmail.com>
2019-11-17 21:15:00 -0800

    update solution statement

--- Grokking_Coding_Interview/slidingWindow/noRepeatSubstring.js
+++ Grokking_Coding_Interview/slidingWindow/noRepeatSubstring.js
@@ -1,5 +1,5 @@
 /*
 No-repeat Substring
 
-Solution: While traversing the string with the right pointer, store the character's index value in an object. When a duplicate character is reached, calculate the longest substring, then move the left pointer to the existing index value + 1 and continue iteration.
+Solution: Use sliding window pattern. While traversing the string with the right pointer, store the character's index value in an object. When a duplicate character is reached, calculate the longest substring, then move the left pointer to the existing index value + 1 and continue iteration.
 */
