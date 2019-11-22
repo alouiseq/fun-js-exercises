c01a9f3ea83171a53de352a9a77bed886d0c6c43
Alouise Quiatchon <alouiseq@gmail.com>
2019-11-22 12:33:45 -0800

    update solution statement

--- Grokking_Coding_Interview/slidingWindow/longestSubstringWithSameLetterReplace.js
+++ Grokking_Coding_Interview/slidingWindow/longestSubstringWithSameLetterReplace.js
@@ -1,7 +1,7 @@
 /*  TODO
-Check if we can sort the string. If so, this will simplify the problem since we won't have to keep track of which characters have been replaced. Let's assume that we cannot sort so that the characters stay in place where they are.
-
-Solution: Use a modified no repeat substring function (returns the range of the longest substring instead of longest value) to find the left and right indices of the longest distinct substring (we'll call distinctString). Then use a left pointer to traverse distinctString from start towards the left side as long as there is enough per the k value. If not, then use the right index to traverse towards the right side of distinctString. Deduct from k and when k === 0, calculate the longest substring.
+Solution: Use the sliding window pattern. Use left and right pointers. Outer loop traverses the string with the right pointer and continues while currentChar === previousChar. If not, deduct 1 from k and continue string traversal as long as k >= 0. Also use a stack to track the next character position index. If the currentChar !== previousChar and k === 0, calculate the longest substring length by rightPointer - leftPointer, and then set the left pointer index by popping from the stack and using that index value. Continue these steps until the right pointe reaches the end of string. Return the longest substring.
+Time: O(n)
+Space: O(n)
 */
 
 const lengthOfLongestSubstring = function(str, k) {
