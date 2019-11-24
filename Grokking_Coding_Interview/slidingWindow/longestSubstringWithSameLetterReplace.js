bba2bbc9efec1ff120021416b7d77753971d5410
Alouise Quiatchon <alouiseq@gmail.com>
2019-11-24 00:18:35 -0800

    improve on space complexity

--- Grokking_Coding_Interview/slidingWindow/longestSubstringWithSameLetterReplace.js
+++ Grokking_Coding_Interview/slidingWindow/longestSubstringWithSameLetterReplace.js
@@ -1,7 +1,7 @@
 /*  TODO
-Solution: Use the sliding window pattern. Use left and right pointers. Outer loop traverses the string with the right pointer and continues while currentChar === previousChar. If not, deduct 1 from k and continue string traversal as long as k >= 0. Also use a stack to track the next character position index. If the currentChar !== previousChar and k === 0, calculate the longest substring length by rightPointer - leftPointer, then set the left pointer index by popping from the stack and using that index value, and lastly increment k. Continue these steps until the right pointe reaches the end of string. Return the longest substring.
+Solution: Use the sliding window pattern similar to longest substring with distinct characters. Use left and right pointers. Outer loop traverses the string with the right pointer and continues while currentChar === previousChar. If not, deduct 1 from k and continue string traversal as long as k >= 0. Also use a stack to track the next character position index. If the currentChar !== previousChar and k === 0, calculate the longest substring length by rightPointer - leftPointer, then set the left pointer index by popping from the stack and using that index value, and lastly increment k. Continue these steps until the right pointe reaches the end of string. Return the longest substring.
 Time: O(n)
-Space: O(n)
+Space: O(n)  Can be reduced to O(26) => O(1) if we do away with the queue and instead use a frequency map to store character/frequency as key/value.
 */
 
 const lengthOfLongestSubstring = function(str, k) {
