c2b508dcc74d4f7480e0808bee92b4b1aa171ac6
Alouise Quiatchon <alouiseq@gmail.com>
2019-11-24 00:23:34 -0800

    add solution statement

---
+++ Grokking_Coding_Interview/slidingWindow/longestSubarrayWithOnesAfterReplace.js
@@ -0,0 +1,8 @@
+/*
+Solution:
+Sliding pattern similar to the longest substring with Same Letter Replacement problem except there are only two types of input character: 0, 1.
+*/
+const length_of_longest_substring = function(arr, k) {
+    // TODO: Write your code here
+    return -1;
+  };
