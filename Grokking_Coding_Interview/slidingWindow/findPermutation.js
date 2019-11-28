5d19f4ac0cbe481b1832cf5def9b9e72c45fc8ce
Alouise Quiatchon <alouiseq@gmail.com>
2019-11-27 17:26:09 -0800

    add solution statement

--- Grokking_Coding_Interview/slidingWindow/findPermutation.js
+++ Grokking_Coding_Interview/slidingWindow/findPermutation.js
@@ -1,3 +1,6 @@
+/* SOLUTION
+Use sliding window pattern similar to longestSubstringWithDistinctCharacters. Use a hashmap to store the pattern. Use 2 ptrs to control the window size and ensure it's the size of the pattern. If moving the window forward, restore the left side data of the window that goes out after moving it. If the variable match === size of hashmap, then return TRUE, otherwise return FALSE after the end of the loop.
+*/
 const find_permutation = function(str, pattern) {
     // TODO: Write your code here
     return false;
