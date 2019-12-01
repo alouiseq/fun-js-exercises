052586aae6fa4a15f4cc86a40cca0796d00ac241
Alouise Quiatchon <alouiseq@gmail.com>
2019-11-30 19:14:01 -0800

    add solution statement to string anagrams

--- Grokking_Coding_Interview/slidingWindow/stringAnagrams.js
+++ Grokking_Coding_Interview/slidingWindow/stringAnagrams.js
@@ -1,3 +1,8 @@
+/* SOLUTION
+Similar to findPermutation, but instead of returning tue when the first match is found, continue to iterate over the entire string looking for all the matches and returning an array of the starting indices of the matches.
+Time: O(n) where n is the length of the input string
+Space: O(m) where m is the length of the pattern
+*/
 const find_string_anagrams = function(str, pattern) {
     result_indexes = [];
     // TODO: Write your code here
