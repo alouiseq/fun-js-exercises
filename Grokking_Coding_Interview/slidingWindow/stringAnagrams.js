e4796d02c4883ae7fdbbafed3efe30934cfe8cd5
Alouise Quiatchon <alouiseq@gmail.com>
2019-12-02 13:10:33 -0800

    add solution logic to string anagrams

--- Grokking_Coding_Interview/slidingWindow/stringAnagrams.js
+++ Grokking_Coding_Interview/slidingWindow/stringAnagrams.js
@@ -4,9 +4,35 @@ Time: O(n) where n is the length of the input string
 Space: O(m) where m is the length of the pattern
 */
 const find_string_anagrams = function(str, pattern) {
-    result_indexes = [];
-    // TODO: Write your code here
-    return result_indexes;
+    const resultIndexes = [];
+    const charFreq = {};
+    let left = 0;
+    let matched = 0;
+    let windowSize = 0;
+
+    // add pattern to hashmap
+    for(let ch of pattern){
+        if(charFreq[ch]) charFreq[ch]++;
+        else charFreq[ch] = 1;
+    }
+
+    for(let right=0; right<str; right++){
+        let curr = str[right];
+        windowSize = right - left + 1;
+        if(curr in charFreq){
+            if(--charFreq[curr] === 0) matched++;
+        }
+        if(windowSize > pattern.length){
+            let prev = str[left++];
+            if(prev in charFreq){
+                if(++charFreq[prev] > 0) matched++
+            }
+        }
+        if(matched === Object.keys(charFreq).length){
+            resultIndexes = [...resultIndexes, left];
+        }
+    }
+    return resultIndexes;
 };
 
 const inputSet = [
