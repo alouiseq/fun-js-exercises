192e4637b4c09b8775fb73a779fe67904dcd5782
Alouise Quiatchon <alouiseq@gmail.com>
2019-12-02 12:43:21 -0800

    add initial input/output setup

--- Grokking_Coding_Interview/slidingWindow/stringAnagrams.js
+++ Grokking_Coding_Interview/slidingWindow/stringAnagrams.js
@@ -1,5 +1,5 @@
 /* SOLUTION
-Similar to findPermutation, but instead of returning tue when the first match is found, continue to iterate over the entire string looking for all the matches and returning an array of the starting indices of the matches.
+Similar to findPermutation, but instead of returning true when the first match is found, continue to iterate over the entire string looking for all the matches and returning an array of the starting indices of the matches.
 Time: O(n) where n is the length of the input string
 Space: O(m) where m is the length of the pattern
 */
@@ -8,3 +8,20 @@ const find_string_anagrams = function(str, pattern) {
     // TODO: Write your code here
     return result_indexes;
 };
+
+const inputSet = [
+    {
+        str: 'ppqp',
+        pattern: 'qp',
+        output: [1,2]
+    },
+    {
+        str: 'abbcabc',
+        pattern: 'abc',
+        output: [2,3,4]
+    },
+];
+
+for(input of inputSet){
+    console.log(input, find_string_anagrams(input.str, input.pattern));
+}
