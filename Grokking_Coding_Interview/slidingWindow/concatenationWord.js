700da576a5e9aa61994cdc95235e8802b38bb878
Alouise Quiatchon <alouiseq@gmail.com>
2019-12-06 13:29:14 -0800

    add concatenationWord problem with input output data

---
+++ Grokking_Coding_Interview/slidingWindow/concatenationWord.js
@@ -0,0 +1,15 @@
+const find_word_concatenation = function(str, words) {
+    result_indices = [];
+    // TODO: Write your code here
+    return result_indices;
+};
+
+const inputSet = [
+    { str: 'catfoxcat', pattern: ['cat', 'fox'], expected: [0, 3] },
+    { str: 'catcatfoxfox', pattern: ['cat', 'fox'], expected: [3] },
+];
+
+for(let input of inputSet){
+    const {str, pattern} = input;
+  console.log('Input', input, 'Output', `'${find_word_concatenation(str, pattern)}'`);
+}
