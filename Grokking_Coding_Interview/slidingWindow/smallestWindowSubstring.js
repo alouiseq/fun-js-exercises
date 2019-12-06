0fc8a145afb996cefaac70574504b71e4f92ce32
Alouise Quiatchon <alouiseq@gmail.com>
2019-12-06 13:19:33 -0800

    add solution statement to smallestWindowSubstring

--- Grokking_Coding_Interview/slidingWindow/smallestWindowSubstring.js
+++ Grokking_Coding_Interview/slidingWindow/smallestWindowSubstring.js
@@ -1,3 +1,16 @@
+/* SOLUTION
+Sliding window method where:
+1. pattern goes into a hashmap.
+2. Maintain a left and right pointers, a matched variable, and a smallest variable.
+3. Use right pointer in the traversal loop.
+4. Check if right index is in hashmap (charFrequency).
+5. If it is then decrement charFrequency and continue loop interation.
+6. If char in charFrequency is 0, then add 1 to match.
+7. If matched === charFrequency length, compare the length of right-left to smallest.length,
+8. If right-left is smaller than smallest, store substring between left and right indices to smallest.
+9. If right pointer is not in charFrequency, increment left index and repeat steps 4-9.
+10. End of loop, return smallest result.
+*/
 const find_substring = function(str, pattern) {
     // TODO: Write your code here
     return "";
