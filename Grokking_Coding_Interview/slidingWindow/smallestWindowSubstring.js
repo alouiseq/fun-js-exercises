b632b14d101f9eb2fc3efddddd6ea7e7c9481ef3
Alouise Quiatchon <alouiseq@gmail.com>
2019-12-10 19:53:23 -0800

    update solution statement

--- Grokking_Coding_Interview/slidingWindow/smallestWindowSubstring.js
+++ Grokking_Coding_Interview/slidingWindow/smallestWindowSubstring.js
@@ -1,10 +1,17 @@
 /* SOLUTION
 Sliding window method and note that the substring can have extra characters since we're looking for the smallest substring where all the characters in the pattern are present:
+Variables: matched, hashmap, smallest, left, right, temp
 1. Put pattern into hashmap.
 2. Outer loop traverses each character in str.
-3.
+3. On each iteration, check if char is in the hashmap.
+4. If it is, then decrement 1 from hashmap.
+5. If not, then continue iteration.
+6. If hashmap[char] >= 0, increment matched, and store in temp.
+7. If matched === pattern.length, compare smallest with temp, and store the smaller of the two.
+8. Shrink the window by incrementing left and repeating 3-8 with the left pointer.
+9. If char is not in hashmap, return smallest.
 Time: O(n + m) where n is str.length and m is pattern.length
-Space: O(n) 
+Space: O(n + m) where n is the chars in the pattern and m is chars in temp.
 */
 const find_substring = function(str, pattern) {
     // TODO: Write your code here
