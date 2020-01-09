3baa4c4a0c5a3570972f3f8afb32597e5fdde30f
Alouise Quiatchon <alouiseq@gmail.com>
2020-01-09 15:42:50 -0800

    fix solution and add time and space

--- Grokking_Coding_Interview/TwoPointers/tripletSumToZero.js
+++ Grokking_Coding_Interview/TwoPointers/tripletSumToZero.js
@@ -1,3 +1,8 @@
+/* Solution:
+Sort the input array for efficient lookup. Run a loop through the sorted array and pass the negative of the value as the target for the inner loop. Use the pairWithTargetSum method as the inner loop and to find the pair that match the target.
+Time: sorting array (nlogn) + [outer loop (n) * pairSumToZero (n)] ==> O(nlogn + n^2) ==> O(n^2)
+Space: O(n) since sorted array n + triplets n
+*/
 const search_triplets = function(arr) {
     triplets = [];
     
@@ -11,19 +16,24 @@ const search_triplets = function(arr) {
     const pairSumToZero = (arr, target, triplets) => {
         let left = 0;
         let right = arr.length - 1;
-        while(left !== right){
+        while(left < right){
             const leftVal = arr[left];
             const rightVal = arr[right];
             const pairSum = leftVal + rightVal;
-            if(target === pairSum) triplets.push([-target, leftVal, rightVal]);
-            else if(target < pairSum) right--;
+            if(leftVal === arr[left-1]){
+                left++;
+                continue;
+            }
+            if(target === pairSum){
+                triplets.push([-target, leftVal, rightVal]);
+                left++;
+            } else if(target < pairSum) right--;
             else if(target > pairSum) left++;
         }
     }
 
     for(let i=0; i<sortedArr.length; i++){
-        if(sortedArr[i] === sortedArr[-1]) continue;
-        pairSumToZero(sortedArr, -sortedArr[i], triplets);
+        pairSumToZero(sortedArr.slice(i+1), -sortedArr[i], triplets);
     }
     return triplets;
 };
