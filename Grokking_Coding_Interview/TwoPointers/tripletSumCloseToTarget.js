a324111723f1b956906e8726196c84dd9925d620
Alouise Quiatchon <alouiseq@gmail.com>
2020-01-10 14:29:37 -0800

    add solution statement

--- Grokking_Coding_Interview/TwoPointers/tripletSumCloseToTarget.js
+++ Grokking_Coding_Interview/TwoPointers/tripletSumCloseToTarget.js
@@ -1,3 +1,13 @@
+/* Questions to the interviewer
+    1. Can the list be sorted?
+    2. Can target sum be a negative value?
+*/
+/* Solution
+Similar to tripletSumToZero, but the differences are that instead of 0, we need to calculate the number needed to get to the target sum and that the target sum may not be met. This means that we need to track the sum for each triplet and only keep the one that's closest to the target sum.
+Time: Assuming that we can sort, then it would be nlogn + (n*n) => O(n^2)
+Space: n (sorted array) => O(n)
+*/
+// TODO
 const triplet_sum_close_to_target = function(arr, target_sum) {
   // TODO: Write your code here
   return -1;
