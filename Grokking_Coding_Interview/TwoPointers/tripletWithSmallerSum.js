f49d2384198ed5c61609f2f5631e46746d88b8e6
Alouise Quiatchon <alouiseq@gmail.com>
2020-01-13 15:27:25 -0800

    add solution statement and big o

--- Grokking_Coding_Interview/TwoPointers/tripletWithSmallerSum.js
+++ Grokking_Coding_Interview/TwoPointers/tripletWithSmallerSum.js
@@ -1,3 +1,9 @@
+/* Solution
+Similar to both tripletSumCloseToTarget and tripletSumToZero except the the desired output should be smaller than the target. Also the indices should be unique within a triplet. To track this, sort the input and skip the values that are the same as the previous value. The best positions for the two pointers should be start and end of the input.
+Time: O(n^2)
+Space: O(n)
+*/
+// TODO
 const triplet_with_smaller_sum = function(arr, target) {
   count = -1;
   // TODO: Write your code here
