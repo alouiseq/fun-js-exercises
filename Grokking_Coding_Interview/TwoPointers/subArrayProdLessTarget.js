93911f5d78efba646a5d0b2ea7aa885d6e59445e
Alouise Quiatchon <alouiseq@gmail.com>
2020-01-14 14:22:14 -0800

    add solution statement to subarray product less than target

--- Grokking_Coding_Interview/TwoPointers/subArrayProdLessTarget.js
+++ Grokking_Coding_Interview/TwoPointers/subArrayProdLessTarget.js
@@ -1,3 +1,8 @@
+/* Solution
+This appears to me that the sliding window pattern can be used instead of two pointers. Let's use a variable to hold a temporary subarray and another variable for the output array. We can use two indices (left and right) where the right will iterate through the input, check the product of all the values within the left and right indices and compare with the target. If it's less, then take the current value[right] and add to the output. Then add the value to the subarray, and push the subarray to the output. If product > target, decrease the window from the left index and make sure to take the value outside of the window and remove it from the subarray, and add to the output. Repeat the step until the end of the input. Return the output array.
+Time: O(n) for the sliding window
+Space: O(n) since the worst case is that only one value is < target at any given point, so it'll take n to store the output. In this case the temp subarray would only hold 1 value at a time.
+*/
 const find_subarrays = function(arr, target) {
   result = [];
   // TODO: Write your code here
