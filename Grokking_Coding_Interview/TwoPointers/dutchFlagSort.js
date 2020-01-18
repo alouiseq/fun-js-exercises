93b3fd324e38042981ac3558369ecc4f9b3e09ae
Alouise Quiatchon <alouiseq@gmail.com>
2020-01-17 16:28:55 -0800

    add incomplete solution

--- Grokking_Coding_Interview/TwoPointers/dutchFlagSort.js
+++ Grokking_Coding_Interview/TwoPointers/dutchFlagSort.js
@@ -4,6 +4,17 @@ Time: O(n)
 Space: O(1)
 */
 const dutch_flag_sort = function(arr) {
+  let l = 0;
+  let r = arr.length-1;
+  let temp = '';
+
+  while(l !== r) {
+    if(arr[l] > arr[r]) [arr[l], arr[r]] = [arr[r], arr[l]];
+    if(arr[l] === 0) l++;
+    if(arr[r] === 2) r--;
+    console.log(arr);
+    // if(lval === rval && lval === 1)
+  }
 };
 
 // ========================
