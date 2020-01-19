3fe30ab05e707fb6690a047c499c9de638f8d5be
Alouise Quiatchon <alouiseq@gmail.com>
2020-01-19 08:52:50 -0800

    update implementation for dutch flag sort

--- Grokking_Coding_Interview/TwoPointers/dutchFlagSort.js
+++ Grokking_Coding_Interview/TwoPointers/dutchFlagSort.js
@@ -4,16 +4,32 @@ Time: O(n)
 Space: O(1)
 */
 const dutch_flag_sort = function(arr) {
-  let l = 0;
-  let r = arr.length-1;
-  let temp = '';
+  let low = 0;
+  let high = arr.length-1;
+  let i = 0;
 
-  while(l !== r) {
-    if(arr[l] > arr[r]) [arr[l], arr[r]] = [arr[r], arr[l]];
-    if(arr[l] === 0) l++;
-    if(arr[r] === 2) r--;
-    console.log(arr);
-    // if(lval === rval && lval === 1)
+  while (low !== high && i < high) {
+    if (arr[low] === 0) {
+      low++;
+      if (low > i) i = low+1;
+      continue;
+    }
+    if (arr[high] === 2) {
+      high--;
+      continue;
+    }
+
+    if (arr[i] === 0){
+      [arr[low], arr[i]] = [arr[i], arr[low]];
+      low++;
+    }
+    if (arr[i] === 2){
+      [arr[high], arr[i]] = [arr[i], arr[high]];
+      high--;
+    }
+    if (arr[i] === 1){
+      i++;
+    }
   }
 };
 
@@ -23,6 +39,7 @@ const dutch_flag_sort = function(arr) {
 const inputSet = [
   { arr: [1, 0, 2, 1, 0], expected: [0, 0, 1, 1, 2] },
   { arr: [2, 2, 0, 1, 2, 0], expected: [0, 0, 1, 2, 2, 2 ] },
+  { arr: [1, 1, 2, 0, 1, 1], expected: [0, 1, 1, 1, 1, 2 ] },
 ];
 
 // Invocation
