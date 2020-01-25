4f7e0005005401e4714c6d3e50edf7fb3286c9b6
Alouise Quiatchon <alouiseq@gmail.com>
2020-01-24 23:01:54 -0800

    add solution to linked list cycle

--- Grokking_Coding_Interview/fastSlowPointers/linkedListCycle.js
+++ Grokking_Coding_Interview/fastSlowPointers/linkedListCycle.js
@@ -11,7 +11,15 @@ class Node {
 }
 
 const has_cycle = function(head) {
-  // TODO: Write your code here
+  let slow = head;
+  let fast = head;
+
+  while(fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
+  }
+
   return false
 }
 
@@ -22,10 +30,10 @@ head.next.next = new Node(3)
 head.next.next.next = new Node(4)
 head.next.next.next.next = new Node(5)
 head.next.next.next.next.next = new Node(6)
-console.log(`LinkedList has cycle: ${has_cycle(head)}`)
+console.log(`LinkedList has cycle: ${has_cycle(head)}`, ' - expected to be false');
 
 head.next.next.next.next.next.next = head.next.next
-console.log(`LinkedList has cycle: ${has_cycle(head)}`)
+console.log(`LinkedList has cycle: ${has_cycle(head)}`, ' - expected to be true');
 
 head.next.next.next.next.next.next = head.next.next.next
-console.log(`LinkedList has cycle: ${has_cycle(head)}`)
+console.log(`LinkedList has cycle: ${has_cycle(head)}`, ' - expected to be true');
