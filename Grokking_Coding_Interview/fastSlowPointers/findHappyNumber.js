7b38ab5eb2b223b4422d336177b66611b30f9494
Alouise Quiatchon <alouiseq@gmail.com>
2020-01-27 14:07:01 -0800

    add middleOfLinkedList problem

--- Grokking_Coding_Interview/fastSlowPointers/findHappyNumber.js
+++ Grokking_Coding_Interview/fastSlowPointers/findHappyNumber.js
@@ -4,5 +4,11 @@ const find_happy_number = function(num) {
 };
 
 
-console.log(`${find_happy_number(23)}`)
-console.log(`${find_happy_number(12)}`)
+console.log(`${find_happy_number(23)} - expected to be TRUE`)
+console.log(`${find_happy_number(12)} - expected to be FALSE`)
+
+/*
+  2^2 + 3^2 = 13
+  1^2 + 3^2 = 10
+  1^2 + 0 = 1
+*/
