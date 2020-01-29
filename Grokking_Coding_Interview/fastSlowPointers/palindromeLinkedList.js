924db8bd9f9f77ea3b90efc9212e7d656bb953f4
Alouise Quiatchon <alouiseq@gmail.com>
2020-01-29 15:16:44 -0800

    add palindromeLinkedList problem

---
+++ Grokking_Coding_Interview/fastSlowPointers/palindromeLinkedList.js
@@ -0,0 +1,24 @@
+class Node {
+  constructor(value, next=null){
+    this.value = value;
+    this.next = next;
+  }
+}
+
+
+const is_palindromic_linked_list= function(head) {
+  // TODO: Write your code here
+  return false;
+};
+
+
+head = new Node(2)
+head.next = new Node(4)
+head.next.next = new Node(6)
+head.next.next.next = new Node(4)
+head.next.next.next.next = new Node(2)
+
+console.log(`Is palindrome: ${is_palindromic_linked_list(head)}`)
+
+head.next.next.next.next.next = new Node(2)
+console.log(`Is palindrome: ${is_palindromic_linked_list(head)}`)
