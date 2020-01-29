7f2e367d5fc9be2c291c9cd5c188aa6726e9185c
Alouise Quiatchon <alouiseq@gmail.com>
2020-01-29 15:37:48 -0800

    add test cases

--- Grokking_Coding_Interview/fastSlowPointers/palindromeLinkedList.js
+++ Grokking_Coding_Interview/fastSlowPointers/palindromeLinkedList.js
@@ -18,7 +18,7 @@ head.next.next = new Node(6)
 head.next.next.next = new Node(4)
 head.next.next.next.next = new Node(2)
 
-console.log(`Is palindrome: ${is_palindromic_linked_list(head)}`)
+console.log(`Is palindrome: ${is_palindromic_linked_list(head)}`, 'expected to be TRUE');
 
 head.next.next.next.next.next = new Node(2)
-console.log(`Is palindrome: ${is_palindromic_linked_list(head)}`)
+console.log(`Is palindrome: ${is_palindromic_linked_list(head)}`, 'expected to be FALSE');
