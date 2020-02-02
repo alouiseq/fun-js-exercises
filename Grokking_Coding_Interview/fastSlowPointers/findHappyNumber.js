682f5f2bb851106d77c55dfe13660b4411c427ec
Alouise Quiatchon <alouiseq@gmail.com>
2020-02-01 22:58:11 -0800

    add an inefficient solution to findHappyNumber

--- Grokking_Coding_Interview/fastSlowPointers/findHappyNumber.js
+++ Grokking_Coding_Interview/fastSlowPointers/findHappyNumber.js
@@ -1,3 +1,8 @@
+/* SOLUTION
+We could store the sum of the squares of the digits into a hash to track which numbers have been visited. If repeatedly calculating the sum of the squares produce a 1 before a number that has been visited, then return true but if the visited occurs first, then return false.
+Time: ?? could be low or high
+Space: linear
+*/
 const find_happy_number = function(num) {
   // TODO: Write your code here
   return false;
