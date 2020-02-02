74a813bbdb58aa3401b71c175d84c487684829ec
Alouise Quiatchon <alouiseq@gmail.com>
2020-02-01 23:12:20 -0800

    add better solution to findHappyNumber

--- Grokking_Coding_Interview/fastSlowPointers/findHappyNumber.js
+++ Grokking_Coding_Interview/fastSlowPointers/findHappyNumber.js
@@ -1,7 +1,12 @@
-/* SOLUTION
+/* SOLUTION 1
 We could store the sum of the squares of the digits into a hash to track which numbers have been visited. If repeatedly calculating the sum of the squares produce a 1 before a number that has been visited, then return true but if the visited occurs first, then return false.
 Time: ?? could be low or high
 Space: linear
+*/
+/* SOLUTION 2 - Better approach
+Use fast and slow pointers. If the number is not a happy number, it ends up in a cycle meaning the two pointers would eventually meet. If the sum of the squares is 1 before they meet, then return true for the happy number, otherwise return false.
+Time: length of the cycle with worst at O(n) but we don't know what n is for the sum of squares ??
+Space: constant
 */
 const find_happy_number = function(num) {
   // TODO: Write your code here
