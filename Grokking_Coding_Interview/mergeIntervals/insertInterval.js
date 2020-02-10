c557ea2583d0dec7db2d8a9b0577e73b7b9527d4
Alouise Quiatchon <alouiseq@gmail.com>
2020-02-10 15:41:59 -0800

    add solution proposal to insertInterval

--- Grokking_Coding_Interview/mergeIntervals/insertInterval.js
+++ Grokking_Coding_Interview/mergeIntervals/insertInterval.js
@@ -1,3 +1,11 @@
+/* Solution
+Assuming the intervals are sorted (if not then sort), iterate the list and compare the new interval with each one to find the correct place for it:
+A. If the newInterval.start < currentInterval.start then insert the new interval before the current and use mergeIntervals() for the list from current to the end of the list.
+B. If newInterval.start > currentInterval.start but newInterval.end < currentInterval.end, then insert the new interval after the current one and use mergeIntervals() for the list from current to the end of the list.
+C. If newInterval.start > currentInterval.start and newInterval.end > currentInterval.end, then insert the currentInterval to the new mergedIntervals and continue the loop iteration.
+Time: O(n) for finding the insertion point + O(n) for merging intervals => O(n)
+Space: O(n) for new list with added interval (or can do in place) + O(n) for merged result => O(n)
+*/
 const insert = function(intervals, new_interval) {
   merged = [];
   // TODO: Write your code here
