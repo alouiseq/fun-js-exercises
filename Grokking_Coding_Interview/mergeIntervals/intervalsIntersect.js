a56d38cd4476aa56c33912b4b25447ebd1eb1395
Alouise Quiatchon <alouiseq@gmail.com>
2020-02-13 07:08:21 -0800

    add intervalsIntersect exercise

---
+++ Grokking_Coding_Interview/mergeIntervals/intervalsIntersect.js
@@ -0,0 +1,17 @@
+const merge = function(intervals_a, intervals_b) {
+  result = [];
+  // TODO: Write your code here
+  return result;
+};
+
+merged_intervals = merge([[1, 3], [5, 6], [7, 9]], [[2, 3], [5, 7]]);
+result = "";
+for(i=0; i < merged_intervals.length; i++)
+  result += "[" + merged_intervals[i][0] + ", " + merged_intervals[i][1] + "] ";
+console.log("Intervals Intersection: " + result);
+
+merged_intervals = merge([[1, 3], [5, 7], [9, 12]], [[5, 10]]);
+result = "";
+for(i=0; i < merged_intervals.length; i++)
+  result += "[" + merged_intervals[i][0] + ", " + merged_intervals[i][1] + "] ";
+console.log("Intervals Intersection: " + result);
