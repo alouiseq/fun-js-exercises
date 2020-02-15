2d5e806b1d44baf392337f63e33458e1aa3d35d7
Alouise Quiatchon <alouiseq@gmail.com>
2020-02-14 16:39:41 -0800

    add expected outputs

--- Grokking_Coding_Interview/mergeIntervals/intervalsIntersect.js
+++ Grokking_Coding_Interview/mergeIntervals/intervalsIntersect.js
@@ -1,3 +1,7 @@
+/* SOLUTION
+Time:
+Space:
+*/
 const merge = function(intervals_a, intervals_b) {
   result = [];
   // TODO: Write your code here
@@ -8,10 +12,10 @@ merged_intervals = merge([[1, 3], [5, 6], [7, 9]], [[2, 3], [5, 7]]);
 result = "";
 for(i=0; i < merged_intervals.length; i++)
   result += "[" + merged_intervals[i][0] + ", " + merged_intervals[i][1] + "] ";
-console.log("Intervals Intersection: " + result);
+console.log("Intervals Intersection: " + result, 'EXPECTED: [[2, 3], [5, 6], [7, 7]]');
 
 merged_intervals = merge([[1, 3], [5, 7], [9, 12]], [[5, 10]]);
 result = "";
 for(i=0; i < merged_intervals.length; i++)
   result += "[" + merged_intervals[i][0] + ", " + merged_intervals[i][1] + "] ";
-console.log("Intervals Intersection: " + result);
+console.log("Intervals Intersection: " + result, 'EXPECTED; [[5, 7], [9, 10]]');
