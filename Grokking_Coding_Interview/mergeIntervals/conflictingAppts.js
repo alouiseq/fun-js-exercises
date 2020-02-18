45fd83267bc1bf1e1a95c6102adbddd1abddca62
Alouise Quiatchon <alouiseq@gmail.com>
2020-02-18 13:37:04 -0800

    add working solution to conflictingAppts

--- Grokking_Coding_Interview/mergeIntervals/conflictingAppts.js
+++ Grokking_Coding_Interview/mergeIntervals/conflictingAppts.js
@@ -4,11 +4,22 @@ Time: O(nlogn) for sorting + O(n) for comparisons so O(n)
 Space: O(1) if using the same array after sorting.
 */
 const can_attend_all_appointments = function(intervals) {
-  // TODO: Write your code here
-  return false;
+  // sort
+  intervals = intervals.sort((a,b) => {
+    if (a[0] === b[0]) return 0;
+    return a[0] < b[0] ? -1 : 1;
+  })
+
+  // find overlap
+  let prev = intervals[0];
+  for(let i=1; i<intervals.length; i++) {
+    if (intervals[i][0] <= prev[1]) return false;
+    prev = intervals[i];
+  }
+  return true;
 };
 
 
-console.log(`Can attend all appointments: ${can_attend_all_appointments([[1, 4], [2, 5], [7, 9]])}`)
-console.log(`Can attend all appointments: ${can_attend_all_appointments([[6, 7], [2, 4], [8, 12]])}`)
-console.log(`Can attend all appointments: ${can_attend_all_appointments([[4, 5], [2, 3], [3, 6]])}`)
+console.log(`Can attend all appointments: ${can_attend_all_appointments([[1, 4], [2, 5], [7, 9]])}`, 'EXPECTED: false')
+console.log(`Can attend all appointments: ${can_attend_all_appointments([[6, 7], [2, 4], [8, 12]])}`, 'EXPECTED: true')
+console.log(`Can attend all appointments: ${can_attend_all_appointments([[4, 5], [2, 3], [3, 6]])}`, 'EXPECTED: false')
