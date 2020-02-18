cbdb8cdd7cd35f2743dcc5e84119d3f87d4b59d3
Alouise Quiatchon <alouiseq@gmail.com>
2020-02-18 08:38:41 -0800

    add conflictingAppts exercise

---
+++ Grokking_Coding_Interview/mergeIntervals/conflictingAppts.js
@@ -0,0 +1,9 @@
+const can_attend_all_appointments = function(intervals) {
+  // TODO: Write your code here
+  return false;
+};
+
+
+console.log(`Can attend all appointments: ${can_attend_all_appointments([[1, 4], [2, 5], [7, 9]])}`)
+console.log(`Can attend all appointments: ${can_attend_all_appointments([[6, 7], [2, 4], [8, 12]])}`)
+console.log(`Can attend all appointments: ${can_attend_all_appointments([[4, 5], [2, 3], [3, 6]])}`)
