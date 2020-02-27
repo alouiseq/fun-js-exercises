f96f3ca00a1f381d1d02d51a7b1304c89a200e5f
Alouise Quiatchon <alouiseq@gmail.com>
2020-02-27 14:47:14 -0800

    add cyclicSort exercise

---
+++ Grokking_Coding_Interview/cyclicSort/cyclicSort.js
@@ -0,0 +1,9 @@
+const cyclic_sort = function(nums) {
+  // TODO: Write your code here
+  return nums;
+}
+
+
+console.log(`${cyclic_sort([3, 1, 5, 4, 2])}`)
+console.log(`${cyclic_sort([2, 6, 4, 3, 1, 5])}`)
+console.log(`${cyclic_sort([1, 5, 6, 4, 3, 2])}`)
