537ec823be6347c86c71be29f3ec4d8b698c8f0e
Alouise Quiatchon <alouiseq@gmail.com>
2020-02-27 22:50:19 -0800

    add solution proposal

--- Grokking_Coding_Interview/cyclicSort/findMissingNumber.js
+++ Grokking_Coding_Interview/cyclicSort/findMissingNumber.js
@@ -1,3 +1,8 @@
+/* SOLUTION
+Similar to cyclic sort except this one adds an additional step of having to iterate the array once more after the values have been placed into the correct positions to find the position where no number exists, hence finding the missing number.
+Time: O(n) array iteration of placing values + O(n-1) swaps + O(n) check for missing positions => O(n)
+Space: O(1)
+*/
 const find_missing_number = function(nums) {
   // TODO: Write your code here
   return -1;
