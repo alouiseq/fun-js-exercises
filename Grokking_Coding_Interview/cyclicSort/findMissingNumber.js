94cc04e04378d1317e385c6dee2630ce257738ed
Alouise Quiatchon <alouiseq@gmail.com>
2020-02-28 17:34:44 -0800

    add working solution to findMissingNumber

--- Grokking_Coding_Interview/cyclicSort/findMissingNumber.js
+++ Grokking_Coding_Interview/cyclicSort/findMissingNumber.js
@@ -4,6 +4,30 @@ Time: O(n) array iteration of placing values + O(n-1) swaps + O(n) check for mis
 Space: O(1)
 */
 const find_missing_number = function(nums) {
-  // TODO: Write your code here
+  let i = 0;
+  let tempVal = null;
+  
+  // add val to nums
+  nums = [...nums, null];
+
+  while (i < nums.length) {
+    if(i === nums[i] || nums[i] === null) i++;
+    else {
+      tempVal = nums[i];
+      [nums[i], nums[tempVal]] = [nums[tempVal], nums[i]];
+      // tempVal = nums[i];
+      // nums[i] = nums[tempVal];
+      // nums[tempVal] = tempVal;
+    }
+  }
+  
+  // find the missing number
+  for(let j=0; j<nums.length; j++) {
+    if(nums[j] === null) return j;
+  }
+
   return -1;
 };
+
+console.log(`ACTUAL: ${find_missing_number([4, 0, 3, 1])} --`, 'EXPECTED: 2')
+console.log(`ACTUAL: ${find_missing_number([8, 3, 5, 2, 4, 6, 0, 1])} --`, 'EXPECTED: 7')
