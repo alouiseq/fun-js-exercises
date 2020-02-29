32b2cbc77e49d49edadc5f60c2087b6d25d570f4
Alouise Quiatchon <alouiseq@gmail.com>
2020-02-28 19:21:16 -0800

    add findMissingNumberts exercise

---
+++ Grokking_Coding_Interview/cyclicSort/findMissingNumbers.js
@@ -0,0 +1,9 @@
+const find_missing_numbers = function(nums) {
+  missingNumbers = [];
+  // TODO: Write your code here
+  return missingNumbers;
+};
+
+console.log(`ACTUAL: ${find_missing_number([2, 3, 1, 8, 2, 3, 5, 1])} --`, 'EXPECTED: 4, 6, 7')
+console.log(`ACTUAL: ${find_missing_number([2, 4, 1, 2])} --`, 'EXPECTED: 3')
+console.log(`ACTUAL: ${find_missing_number([2, 3, 2, 1])} --`, 'EXPECTED: 4')
