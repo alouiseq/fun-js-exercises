fa540c9a2246c711b2b5c1b2fca9404b2af0ac6b
Alouise Quiatchon <alouiseq@gmail.com>
2020-03-01 09:29:39 -0800

    add findDuplicate exercise

---
+++ Grokking_Coding_Interview/cyclicSort/findDuplicate.js
@@ -0,0 +1,13 @@
+/* SOLUTION
+TIME:
+SPACE:
+*/
+const find_duplicate = function(nums) {
+  // TODO: Write your code here
+  return -1;
+};
+
+console.log(`ACTUAL: ${find_missing_numbers([1, 4, 4, 3, 2])} --`, 'EXPECTED: 4')
+console.log(`ACTUAL: ${find_missing_numbers([2, 1, 3, 3, 5, 4])} --`, 'EXPECTED: 3')
+
+console.log(`ACTUAL: ${find_missing_numbers([2, 4, 1, 4, 4])} --`, 'EXPECTED: 4')
