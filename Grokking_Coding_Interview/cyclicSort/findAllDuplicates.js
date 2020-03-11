fd69e976d94de71d8087c73d003e4341532337f8
Alouise Quiatchon <alouiseq@gmail.com>
2020-03-11 14:33:08 -0700

    add findAllDuplicates exercise

---
+++ Grokking_Coding_Interview/cyclicSort/findAllDuplicates.js
@@ -0,0 +1,13 @@
+/* SOLUTION
+Similar to findDuplicate, but instead use an array to store the dupicates and ensure iteration of the input array until the end of the loop.
+Time: O(n)
+Space: O(1)
+*/
+const find_all_duplicates = function(nums) {
+  duplicateNumbers = [];
+  // TODO: Write your code here
+  return duplicateNumbers;
+};
+
+console.log(`ACTUAL: ${find_duplicate([3, 4, 4, 5, 5])} --`, 'EXPECTED: [4, 5]')
+console.log(`ACTUAL: ${find_duplicate([5, 4, 7, 2, 3, 5, 3])} --`, 'EXPECTED: [3, 5]')
--- Grokking_Coding_Interview/cyclicSort/findDuplicate.js
+++ Grokking_Coding_Interview/cyclicSort/findDuplicate.js
@@ -22,5 +22,4 @@ const find_duplicate = function(nums) {
 
 console.log(`ACTUAL: ${find_duplicate([1, 4, 4, 3, 2])} --`, 'EXPECTED: 4')
 console.log(`ACTUAL: ${find_duplicate([2, 1, 3, 3, 5, 4])} --`, 'EXPECTED: 3')
-
 console.log(`ACTUAL: ${find_duplicate([2, 4, 1, 4, 4])} --`, 'EXPECTED: 4')
