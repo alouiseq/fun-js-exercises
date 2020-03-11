399d08fa76309b4ee9d6c13f65416933e18fd7d0
Alouise Quiatchon <alouiseq@gmail.com>
2020-03-11 15:01:10 -0700

    add working solution to findAllDuplicates

--- Grokking_Coding_Interview/cyclicSort/findAllDuplicates.js
+++ Grokking_Coding_Interview/cyclicSort/findAllDuplicates.js
@@ -5,9 +5,20 @@ Space: O(1)
 */
 const find_all_duplicates = function(nums) {
   duplicateNumbers = [];
-  // TODO: Write your code here
+  let i = 0, j = 0;
+
+  while (i < nums.length) {
+    j = nums[i] - 1;
+    if (nums[i] === i+1) i++;
+    else if (nums[i] === nums[j]) {
+      duplicateNumbers.push(nums[i]);
+      i++;
+    } else {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+  }
   return duplicateNumbers;
 };
 
-console.log(`ACTUAL: ${find_duplicate([3, 4, 4, 5, 5])} --`, 'EXPECTED: [4, 5]')
-console.log(`ACTUAL: ${find_duplicate([5, 4, 7, 2, 3, 5, 3])} --`, 'EXPECTED: [3, 5]')
+console.log(`ACTUAL: ${find_all_duplicates([3, 4, 4, 5, 5])} --`, 'EXPECTED: [4, 5]')
+console.log(`ACTUAL: ${find_all_duplicates([5, 4, 7, 2, 3, 5, 3])} --`, 'EXPECTED: [3, 5]')
