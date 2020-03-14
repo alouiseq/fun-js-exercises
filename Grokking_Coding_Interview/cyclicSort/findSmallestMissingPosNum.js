65f9c0aefa9a3faf4749f76f7ced2de9ef6318f4
Alouise Quiatchon <alouiseq@gmail.com>
2020-03-14 00:56:43 -0700

    add findSmallestMissingPosNum exercise

---
+++ Grokking_Coding_Interview/cyclicSort/findSmallestMissingPosNum.js
@@ -0,0 +1,12 @@
+/* SOLUTION
+Time: O(n) + O(n) => O(n)
+Space: O(1)
+*/
+const find_first_missing_positive = function(nums) {
+  // TODO: Write your code here
+  return -1;
+};
+
+console.log(`ACTUAL: ${find_first_missing_positive([-3, 1, 5, 4, 2])} --`, 'EXPECTED: 3')
+console.log(`ACTUAL: ${find_first_missing_positive([3, -2, 0, 1, 2])} --`, 'EXPECTED: 4')
+console.log(`ACTUAL: ${find_first_missing_positive([3, 2, 5, 1])} --`, 'EXPECTED: 4')
