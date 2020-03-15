84fbc8e919719602500c1aba8b06262559aa56ff
Alouise Quiatchon <alouiseq@gmail.com>
2020-03-15 12:20:54 -0700

    add working solution to findCorruptPair

--- Grokking_Coding_Interview/cyclicSort/findCorruptPair.js
+++ Grokking_Coding_Interview/cyclicSort/findCorruptPair.js
@@ -4,9 +4,36 @@ Time: O(n)
 Space: O(1)
 */
 const find_corrupt_numbers = function(nums) {
-  // TODO: Write your code here
-  return [-1, -1];
+  const corrupts = [-1, -1];
+  let i = 0, j = 0;
+
+  // find duplicate 
+  while (i < nums.length) {
+    j = nums[i] - 1;
+    if (nums[i] === i+1) i++;
+    else if (nums[i] === nums[j]) {
+      corrupts[0] = nums[i];
+      i++;
+    } else {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+  }
+
+  // find missing number
+  for (let k=0; k<nums.length; k++) {
+    if (k+1 !== nums[k]) {
+      corrupts[1] = k+1;
+      break;
+    }
+  }
+
+  return corrupts;
 };
 
-console.log(`ACTUAL: ${find_corrupt_numbers([3, 1, 2, 5, 2])} --`, 'EXPECTED: [2, 4]')
-console.log(`ACTUAL: ${find_corrupt_numbers([3, 1, 2, 3, 6, 4])} --`, 'EXPECTED: [3, 5]')
+console.log(`ACTUAL: [${find_corrupt_numbers([3, 1, 2, 5, 2])}] --`, 'EXPECTED: [2, 4]')
+console.log(`ACTUAL: [${find_corrupt_numbers([3, 1, 2, 3, 6, 4])}] --`, 'EXPECTED: [3, 5]')
+
+// [3, 1, 2, 5, 2]
+// [2, 1, 3, 5, 2]
+// [1, 2, 3, 5, 2]
+// [1, 2, 3, 2, 5]
